899466b9eb1512ae766bf8b2b99438bf2e4655d3
Adesh Jadhav <adejad1234@gmail.com>
2024-06-08 15:18:25 +0530

    Added following routes: -> search route -> membership route

--- backend/schema/customerModel.js
+++ backend/schema/customerModel.js
@@ -19,7 +19,9 @@ const customerSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    unique: true,
+    unique: [true, "Phone no already taken"],
+    minlength: 10,
+    maxlength: 10,
   },
   dateOfBirth: {
     type: Date,
@@ -65,7 +67,6 @@ function validateCustomer(customer) {
     phone: Joi.string()
       .required()
       .trim()
-      .unique()
       .pattern(/^[0-9]+$/),
     dateOfBirth: Joi.date().required(),
     status: Joi.string().valid("active", "inactive").default("active"),
